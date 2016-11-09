6246aa843a1bd627aafc2213c2eaa41e96fcc2bc
Nanderson Castro <nandokstro@gmail.com>
2016-11-09 05:36:56 -0300

    Estudos Angular2

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+import { MeuPrimeiroComponente } from './primeiro/meu-primeiro.component'
+
+
 @Component({
     selector: 'my-app',
     template: `
         <h1>Angular 2 Boilerplate</h1>
         <p>Hello World!</p>
-    `
+        <meu-primeiro-componente></meu-primeiro-componente>
+    `,
+    directives: [MeuPrimeiroComponente]
 })
 export class AppComponent { }
